8e472cf85b0b30a887f6861dcd2df710d14828c7
Humza920 <humza.rehman.779@gmail.com>
2024-03-14 04:23:59 -0700

    HALF

---
+++ app.js
@@ -0,0 +1,39 @@
+// QUESTION 1 <==literal notation
+// const studentNames = []
+
+// QUESTION 2 <==object notation
+// const studentNames = {
+//     names:['HUMZA' , "BILAL" , "ASHAR"]
+// }
+// console.log(studentNames);
+
+// QUESTION 3
+// const stringsArray = ["HUMZA" , "BILAL" , "ASHAR"]
+// console.log(stringsArray);
+
+// QUESTION 4
+// const numberArray = [1,2,3,4,5,6,7]
+// console.log(numberArray);
+
+// QUESTION 5
+// const boleanArray = [true,false,true,false]
+// console.log(boleanArray);
+
+// QUESTION 6
+// const mixedArray = [true,false,true,false,1,2,3,4,5,6,7,"HUMZA" , "BILAL" , "ASHAR"]
+// console.log(mixedArray);
+
+// QUESTION 7
+// const pakNetworks = [`ZONG` , `U-FONE` , `TELENORE`]
+// console.log(pakNetworks);
+
+// QUESTION 8
+// document.write("QUALIFICATIONs")
+// const education =['SSC', 'HSC', 'BCS', 'BS', 'BCOM', 'MS', 'M. Phil', 'PhD']
+// const ul = document.querySelector("ol")
+// console.log(education);
+// for(let i=0 ; i<education.length ; i++)
+// {ul.innerHTML+=(`<li>${education[i]}</li>`)}
+
+// QUESTION 9
+
