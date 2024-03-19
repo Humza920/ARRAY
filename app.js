7ddb01079bc11b1d6fed80d7c45dffb4605b3116
Humza920 <humza.rehman.779@gmail.com>
2024-03-19 03:41:44 -0700

    HALF

--- app.js
+++ app.js
@@ -28,12 +28,41 @@
 // console.log(pakNetworks);
 
 // QUESTION 8
-// document.write("QUALIFICATIONs")
+// document.write("<h1>QUALIFICATIONs</h1>")
 // const education =['SSC', 'HSC', 'BCS', 'BS', 'BCOM', 'MS', 'M. Phil', 'PhD']
 // const ul = document.querySelector("ol")
 // console.log(education);
 // for(let i=0 ; i<education.length ; i++)
 // {ul.innerHTML+=(`<li>${education[i]}</li>`)}
 
-// QUESTION 9
+// WAY 2
+// document.write("<h1>QUALIFICATION:</h1>" + "<br>")
+// const education = []
+// education.push  (`1)'SSC'`)
+// education.push  (`2)'HSC'`)
+// education.push  (`3)'BSC'`)
+// education.push  (`4)'BS'`)
+// education.push  (`5)'B. COM'`)
+// education.push  (`6)'MS'`)
+// education.push  (`7)'M. PHIL'`)
+// education.push  (`8)'PHD'`)
+// console.log(education);
+// document.write(education.join("</br>"))
 
+// QUESTION 9
+// document.write(`<h1>TOP MOVIES OF 2015</h1>`)
+// const ol = document.querySelector("ol")
+// const movies = [`AGE OF ULTRON` , `JURASSIC PARK` , `SPECTRE` , `INSIDE OUT`] 
+// for(i=0 ; i<movies.length ; i++){
+//    ol.innerHTML+=(`<li>${movies[i]}</li>`)
+// }
+
+// WAY 2
+// document.write("<h1>TOP MOVIES OF 2015</h1>")
+// const movies = []
+// movies.push("1) AVENGERS:Age Of Ultron")
+// movies.push("2) SPECTRE")
+// movies.push("3) JURASSIC WORLD ")
+// movies.push("4) INSIDE OUT ")
+// console.log(movies);
+// document.write(movies.join("</br>"))
