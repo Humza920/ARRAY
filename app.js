3370b49ddf0f294ff8a3d89f55c8f1dc77e97f6f
Humza920 <humza.rehman.779@gmail.com>
2024-05-09 16:20:16 -0700

    DONE

--- app.js
+++ app.js
@@ -134,13 +134,140 @@ for (let i = 0; i < arr.length; i++) {
 // for (let i = 0; i < arr.length; i++) {
 //     uul.innerHTML +=`<li>${arr[i]}</li>`}
 
-// f
-  const uL = document.querySelector(".ul")
-  const input =document.querySelector("input")
-  const inp =document.querySelector(".inp")
-  function add () {
-     for (let i = 0; i < arr.length; i++) {
-         uL.innerHTML +=`<li>${arr[i]}</li>`}
+// f not done
+//   const uL = document.querySelector(".ul")
+//   const input =document.querySelector("input")
+//   const inp =document.querySelector(".inp")
+//   function add () {
+//      for (let i = 0; i < arr.length; i++) {
+//          uL.innerHTML +=`<li>${arr[i]}</li>`}
 
-         inp.value = ""
-}
+//          inp.value = ""
+// }
+
+// g
+//   const uL = document.querySelector(".ul")
+//   const input =document.querySelector("#how much")
+//   const inp =document.querySelector(".inp")
+//   function add () {
+//     arr.splice(inp.value , input.value)
+//      for (let i = 0; i < arr.length; i++) {
+//          uL.innerHTML +=`<li>${arr[i]}</li>`}
+//          inp.value = ""
+//          input.value = ""
+//  }
+
+// QUESTION NO 13 :
+
+// 13. Write a program to store student scores in an array & sort 
+// the array in ascending order using Array’s sort method. 
+
+// SOLUTION :
+
+
+// function getScore() {
+//     let student = [sco1.value, sco2.value, sco3.value, sco4.value, sco5.value, sco6.value, sco7.value, sco8.value, sco9.value, sco10.value];
+//     console.log(student);
+// student.sort(function(a,b){return a-b});
+// console.log(student);
+// }
+
+
+// QUESTION NO 14 :
+
+// 14. Write a program to sort the below mentioned array:
+// var fruits = [“strawberry”, “apple”, “orange”, “banana”];
+
+// SOLUTION :
+
+
+// let fruits = ['strawberry', 'apple', 'orange', 'banana'];
+
+// para.innerHTML = `<h2>Fruits List :</h2>   ${fruits}<br> <h2>Ordered  Fruits List are :</h2> ${(fruits.sort())}`
+
+
+// QUESTION NO 14 :
+
+// 15. Write a program to initialize an array with city names. Copy 
+// 3 array elements from cities array to selectedCities array.
+
+// SOLUTION :
+
+// let cities = ['Karachi','Lahore','Quetta','Islamabad','Peshawar']
+
+
+
+// QUESTION NO 16 :
+
+// 16. Write a program to create a single string from the below 
+// mentioned array:
+// var arr = [“This ”, “ is ”, “ my ”, “ cat”];
+// (Use array’s join method)
+
+// SOLUTION :
+
+
+
+// let arr = ['This' , ' is' ,  'my ', 'cat'];
+// para.innerHTML = `<h2>ARRAY :</h2> ${arr} <br> <h2>String:</h2> ${arr.join(' ')}`
+
+
+// QUESTION NO 17 :
+// 17. Create a new array. Store values one by one in such a way 
+// that you can access the values in the order in which they were 
+// stored. (FIFO-First In First Out)
+
+// SOLUTION :
+
+// let newArr = ['keyboard','mouse','printer','monitor'];
+// console.log(newArr);
+// newArr.shift('power')
+// console.log(newArr);
+
+// para.innerHTML =`<h2>Devices:</h2> <b>Out:</b><br>${newArr[0]} <br> <b>Out:</b><br>${newArr[1]} <br> <b>Out:</b><br>${newArr[2]} <br> <b>Out:</b><br>${newArr[3]}`
+
+
+// QUESTION NO 18 :
+// 18. Create a new array. Store values one by one in such a way 
+// that you can access the values in reverse order. (Last In-First 
+// Out)
+
+// SOLUTION :
+
+// let newArr = ['keyboard','mouse','printer','monitor'];
+
+// para.innerHTML =`<h2>Devices:</h2> <b>Out:</b><br>${newArr[3]}<br>  <br> <b>Out:</b><br>${newArr[2]}<br>  <br> <b>Out:</b><br>${newArr[1]}<br>  <br> <b>Out:</b><br>${newArr[0]}<br> `
+
+
+// QUESTION NO 20 :
+
+// 20. Declare and initialize an empty multidimensional array.
+// (Array of arrays)
+
+// SOLUTION :
+
+// let multidimensional = [[],[],[]];
+
+
+
+// QUESTION NO 20 :
+
+// 21. Declare and initialize a multidimensional array representing
+// the following matrix:
+
+// SOLUTION :
+
+let multidimensionalArr = [['0','1','2','3'],['1','0','1','2'],['2','1','0','1']];
+console.log(multidimensionalArr);
+console.log(multidimensionalArr[0],[1],[2]);
+
+
+
+
+
+
+
+
+
+
+
